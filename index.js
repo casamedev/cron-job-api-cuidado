76bb8f17178d79d2eb49961d42b3159185616124
C0D8 <matheusaguiar362@gmail.com>
2024-03-08 23:12:27 -0300

    a

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const axios = require('axios');
 require('dotenv').config(); // Load environment variables from .env file
 
-cron.schedule('* * * * *', async () => {
+cron.schedule('0 0 * * *', async () => {
     console.log('Running a task every minute');
 
     try {
